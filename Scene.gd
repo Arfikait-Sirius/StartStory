49094c22b12a1f668c8ab302fb794ebb4d6627f6
Kamidama <masarulock01@gmail.com>
2021-12-10 10:23:07 +0900

    スクリプト名変更

--- Scene.gd
+++ Scene.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-export var SCENE_NAME = ""
-
 var scene = preload("res://SceneManager.tscn").instance()
 var text
 var root
@@ -9,10 +7,10 @@ var root_text
 var is_end
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text = scene.scene_dictionary[SCENE_NAME][scene.LIST.TEXT]
-	root = scene.scene_dictionary[SCENE_NAME][scene.LIST.ROOT]
-	root_text = scene.scene_dictionary[SCENE_NAME][scene.LIST.ROOT_TEXT]
-	is_end = scene.scene_dictionary[SCENE_NAME][scene.LIST.END]
+	text = scene.scene_dictionary[self.name][scene.LIST.TEXT]
+	root = scene.scene_dictionary[self.name][scene.LIST.ROOT]
+	root_text = scene.scene_dictionary[self.name][scene.LIST.ROOT_TEXT]
+	is_end = scene.scene_dictionary[self.name][scene.LIST.END]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
